4ce9003cf45a7a8b747a71dd45f4f6787171ea95
onjoujitoki <z@moon.moe>
2021-12-01 14:20:31 -0500

    updated icon

--- src/test/java/edu/neu/csye6200/person/JframeTest.java
+++ src/test/java/edu/neu/csye6200/person/JframeTest.java
@@ -1,10 +1,13 @@
 package edu.neu.csye6200.person;
 import javax.swing.*;
 import java.awt.*;
-
+import java.net.URL;
 public class JframeTest {
     public static void main(String[] args) {
         JFrame window = new JFrame("DayCare System");
+        URL iconPath = JframeTest.class.getClassLoader().getResource("icon.png");
+        Image iconImage = new ImageIcon(iconPath).getImage();
+        window.setIconImage(iconImage);
         window.setSize(600, 400); // in px
         window.setVisible(true);
         window.setResizable(false); // make it nonchangeable
